9a51eeba980b5351814d48840e91e1838aeb46a0
Awasum Yannick <awasum.yannick@skylabase.com>
2017-09-05 17:52:07 +0100

    Generate REST docs for Home Controller test

--- src/test/java/com/skylabase/docs/HomeControllerTest.java
+++ src/test/java/com/skylabase/docs/HomeControllerTest.java
@@ -2,6 +2,10 @@ package com.skylabase.docs;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -26,6 +30,8 @@ public class HomeControllerTest {
 	@Test
 	public void shouldReturnDefaultMessage() throws Exception {
 		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("Hello World"))).andDo(document("home"));
+				.andExpect(content().string(containsString("Hello World")))
+				.andDo(document("home", responseFields(fieldWithPath("message").description("The welcome message for the user.")
+					    )));
 	}
 }
